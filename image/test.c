37ec3e9086169d9dc418998734d9122d4dbda861
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-05 07:25:42 +0900

    ..

---
+++ image/test.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <SDL.h>
+#include <SDL_image.h>
+#ifdef PLATFORM_EMCC
+#include <emscripten.h>
+#endif
+#include <unistd.h>
+#include <stdlib.h>
+
+int _isQuit = 0;
+typedef struct {
+  int dummy;
+} Context;
+
+Context ctx;
+void main_loop(void*args) {
+  SDL_Event event;
+  while (SDL_PollEvent(&event)) {
+    switch (event.type) {
+      case SDL_QUIT:
+        _isQuit = 1;
+    }
+  }
+}
+
+
+int testImage(SDL_Renderer* renderer, const char* fileName)
+{
+  SDL_Surface *image = IMG_Load(fileName);
+  if (!image)
+  {
+     printf("IMG_Load: %s\n", IMG_GetError());
+     return 0;
+  }
+  int result = image->w;
+
+  SDL_Rect dest = {.x = 200, .y = 100, .w = 200, .h = 200};
+
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, image);
+
+  SDL_RenderCopy (renderer, tex, NULL, &dest);
+
+  SDL_DestroyTexture (tex);
+
+  SDL_FreeSurface (image);
+
+  return result;
+}
+
+
+
+int main()
+{
+  SDL_Init(SDL_INIT_VIDEO);
+
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+
+  SDL_CreateWindowAndRenderer(600, 400, 0, &window, &renderer);
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderClear(renderer);
+
+  //
+  // Draw Image
+  SDL_Surface *image = IMG_Load("./image/assets/icon.png");
+  if (!image)
+  {
+     printf("Failed at IMG_Load: %s\n", IMG_GetError());
+     return 0;
+  }
+  SDL_Rect dest = {.x = 0, .y = 0, .w = 100, .h = 100};
+  SDL_Texture *tex = SDL_CreateTextureFromSurface(renderer, image);
+  SDL_RenderCopy (renderer, tex, NULL, &dest);
+//  SDL_DestroyTexture (tex);
+//  SDL_FreeSurface (image);
+
+
+  SDL_RenderPresent(renderer);
+
+  #ifdef PLATFORM_EMCC
+    emscripten_set_main_loop_arg(main_loop, &ctx, 60, 1);
+  #else
+    do {
+      main_loop(&ctx);
+    } while(_isQuit == 0);
+  #endif
+  return 0;
+}
