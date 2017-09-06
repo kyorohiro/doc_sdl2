3d9d306cbbad8c04641d713aa7911c4192ee07e8
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-07 00:20:30 +0900

    ..

--- sdl1_createWindow/test.c
+++ sdl1_createWindow/test.c
@@ -8,11 +8,20 @@ int main( int argc, char* args[] )
 {
   SDL_Init( SDL_INIT_EVERYTHING );
   SDL_Surface* screen = NULL;
-  screen = SDL_SetVideoMode( 640, 480, 32, SDL_SWSURFACE );
+  screen = SDL_SetVideoMode( 640, 480, 0, SDL_HWSURFACE | SDL_DOUBLEBUF );
+  SDL_WM_SetCaption( "title", 0 );
   SDL_Flip( screen );
 #ifdef PLATFORM_EMCC
 #else
-  SDL_Delay( 2000 );
+  SDL_Event event;
+  int isQuit = 0;
+  while(isQuit == 0) {
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        isQuit = 1;
+      }
+    }
+  }
   SDL_Quit();
 #endif
   return 0;
