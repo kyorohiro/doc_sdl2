547b7c73db0366c0cf9e040756f39442dec51a2e
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-15 09:03:07 +0900

    ..

--- sdl1_gl_triangle/test.c
+++ sdl1_gl_triangle/test.c
@@ -1,43 +1,37 @@
-
-#define GL_GLEXT_PROTOTYPES
+//
+// FONT
+// https://github.com/kripken/emscripten/issues/2708
+// https://groups.google.com/forum/#!topic/emscripten-discuss/lfXaVdyYztw
+// http://www.sdltutorials.com/sdl-ttf
+//
 #include <SDL.h>
-#include <SDL_events.h>
+#include <SDL_image.h>
 #include <SDL_opengl.h>
-#include <stdio.h>
 
+#include <stdio.h>
 #ifdef PLATFORM_EMCC
 #include <emscripten.h>
 #endif
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+GLuint cglu_loadShader(GLenum type, const char *shaderSrc);
 
-int _isQuit = 0;
 typedef struct {
   int dummy;
 } Context;
+
 Context ctx;
+SDL_Surface* screen = NULL;
 
-GLuint cglu_loadShader(GLenum type, const char *shaderSrc);
-
-void main_loop(void*args) {
-  SDL_Event event;
-  while (SDL_PollEvent(&event)) {
-    switch (event.type) {
-      case SDL_QUIT:
-        _isQuit = 1;
-    }
-  }
-}
-
-
-int fps;
 int frgShader;
 int verShader;
 int program;
 int width = 400;
 int height = 300;
+
+void main_loop(void*args) {
+}
+
+
 
 GLfloat vertexBufferData[] = {
   0.0f, 0.5f, 0.0f,
@@ -105,35 +99,43 @@ void _onDisplay() {
 
 }
 
-int main()
+int main( int argc, char* args[] )
 {
-  SDL_Init(SDL_INIT_VIDEO);
-
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-
-  SDL_CreateWindowAndRenderer(600, 400, 0, &window, &renderer);
-
-  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-  SDL_RenderClear(renderer);
-
+  printf("main \r\n");
+  SDL_Init( SDL_INIT_EVERYTHING );
+//  screen = SDL_SetVideoMode( 640, 480, 0, SDL_HWSURFACE | SDL_DOUBLEBUF );
+  screen = SDL_SetVideoMode( 640, 480, 32, SDL_HWSURFACE|SDL_OPENGL| SDL_DOUBLEBUF);
+  if(screen == NULL) {
+    printf("Failed at SDL_SetVideoMode\r\n");
+    return 0;
+  }
+  SDL_WM_SetCaption( "title", 0 );
   //
   //
-
+  printf("ttf init \r\n");
   _onInit();
   _onDisplay();
+  SDL_GL_SwapBuffers();//SDL_Flip( screen );
 
+  printf("ttf \r\n");
   //
   //
-  SDL_RenderPresent(renderer);
-
-  #ifdef PLATFORM_EMCC
-    emscripten_set_main_loop_arg(main_loop, &ctx, 60, 1);
-  #else
-    do {
-      main_loop(&ctx);
-    } while(_isQuit == 0);
-  #endif
+#ifdef PLATFORM_EMCC
+  emscripten_set_main_loop_arg(main_loop, &ctx, 60, 1);
+#else
+  SDL_Event event;
+  int isQuit = 0;
+  while(isQuit == 0) {
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        isQuit = 1;
+      }
+    }
+    main_loop(&ctx);
+    SDL_Delay(10);
+  }
+  SDL_Quit();
+#endif
   return 0;
 }
 
@@ -169,3 +171,6 @@ GLuint cglu_loadShader(GLenum type, const char *shaderSrc) {
 
   return shader;
 }
+
+
+
