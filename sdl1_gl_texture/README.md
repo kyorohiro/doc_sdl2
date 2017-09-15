eddc7544f0ec0d59d0967726743b6fbdeb0e8994
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-15 09:26:15 +0900

    ..

---
+++ sdl1_gl_texture/README.md
@@ -0,0 +1,4 @@
+# CreateWindow
+
+{% codeeditor src='./test.c', language='javascript', readOnly='true', theme='github'  %}{% endcodeeditor %}
+{% emscripten js="test.js", mem="test.html.js" %}{% endemscripten %}
--- sdl1_gl_texture/test.c
+++ sdl1_gl_texture/test.c
@@ -31,37 +31,52 @@ int program;
 int width = 400;
 int height = 300;
 
-void main_loop(void*args) {
-}
-
-
-
 GLfloat vertexBufferData[] = {
-  0.0f, 0.5f, 0.0f,
-  -0.5f, -0.5f, 0.0f,
-  0.5f, -0.5f, 0.0f
+  0.0f, 0.5f, 0.0f,   0.0, 0.0,
+  -0.5f, -0.5f, 0.0f, 1.0, 0.0,
+  0.5f, -0.5f, 0.0f,  0.0, 1.0
 };
 
 GLshort indexData[] = {
   0,1,2
 };
 
+void main_loop(void*args) {
+}
+
+
+
+
+
 void _onInit() {
   printf("## onInit\r\n");
 
   glEnable(GL_DEPTH_TEST);
+
+  //glEnable(GL_ALPHA_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   glViewport(0, 0, width, height);
   glClearColor(1.0, 0.7, 0.7, 1.0);//rgba
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
   //
 
   frgShader = cglu_loadShader(GL_FRAGMENT_SHADER,
+    "#ifdef GL_ES\n"
+    "precision mediump float;\n"
+    "#endif\n"
+    "uniform sampler2D texture;\n"
+    "varying vec2 textureCoord;\n"
     "void main() {\n"
-    "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "  gl_FragColor = texture2D(texture, textureCoord)* vec4(1.0,1.0,1.0,1.0);\n"
+//    "  gl_FragColor = vec4(1.0,1.0,1.0,1.0);\n"
     "}\n");
   verShader = cglu_loadShader(GL_VERTEX_SHADER,
     "attribute vec4 position;\n"
+    "attribute vec2 texCoord;\n"
+    "varying vec2 textureCoord;\n"
     "void main() {\n"
+    "  textureCoord = texCoord;\n"
     "  gl_Position = position;\n"
     "}\n");
 
@@ -73,27 +88,76 @@ void _onInit() {
 }
 
 void _onDisplay() {
+  //
+  //
+  int texture;
+  SDL_Surface *image = IMG_Load("./assets/icon.png");
+  if (!image)
+  {
+     printf("Failed at IMG_Load: %s\n", IMG_GetError());
+     return ;
+  }
+
 
   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+
+  // buffer
   GLuint vertexBuffer;
   GLuint indexBuffer;
+  GLuint textureBuffer;
   glGenBuffers(1, &vertexBuffer);
   glGenBuffers(1, &indexBuffer);
+  glGenTextures(1,&textureBuffer);
 
+  // texture
+  GLenum data_fmt;
+  GLint nOfColors = image->format->BytesPerPixel;
+  if (nOfColors == 4) {
+    if (image->format->Rmask == 0x000000ff) {
+      data_fmt = GL_RGBA;
+    } else {
+      data_fmt = GL_BGRA;
+    }
+  } else if (nOfColors == 3) {
+    if (image->format->Rmask == 0x000000ff){
+      data_fmt = GL_RGBA;
+    } else {
+      data_fmt = GL_BGRA;
+    }
+  } else {
+    printf("warning: the image is not truecolor..  this will probably break\n");
+  }
+  glBindTexture(GL_TEXTURE_2D, textureBuffer);
+//  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+//  glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, data_fmt,//GL_RGBA,//data_fmt,
+      image->w, image->h, 0, data_fmt, GL_UNSIGNED_BYTE, image->pixels);
+ // glGenerateMipmap(GL_TEXTURE_2D);
+//  printf("%d %d %d\r\n", image->w, image->h, test);
+  SDL_FreeSurface(image);
+  //
+  // shader
   glUseProgram(program);
   int positionLoc = glGetAttribLocation(program, "position");
+  int texCoordLoc = glGetAttribLocation(program, "texCoord");
+  glEnableVertexAttribArray(positionLoc);
+  glEnableVertexAttribArray(texCoordLoc);
+
 
   //
   glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*3*3, vertexBufferData, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*5*3, vertexBufferData, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(positionLoc, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)0);
+  glVertexAttribPointer(texCoordLoc, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (void*)(3* sizeof(GLfloat)));
 
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLshort)*3, indexData, GL_STATIC_DRAW);
-
-
-  glEnableVertexAttribArray(positionLoc);
-  glVertexAttribPointer(positionLoc, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void*)0);
-
   glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
 
   glDeleteBuffers(1, &vertexBuffer);
@@ -101,6 +165,7 @@ void _onDisplay() {
 
 
 }
+
 
 int main( int argc, char* args[] )
 {
